489b8c0ee609eb4523c1b9856208463cc67fdd69
iamzohav <bjohrabanu@gmail.com>
2025-04-03 21:36:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from my CI/CD Pipeline!"
+    return "Hello from my CI/CD without jenkins Pipeline!"
 
 if __name__ == '__main__':
     print("Running on 0.0.0.0:5000", file=sys.stderr)
